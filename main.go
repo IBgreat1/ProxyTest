a229a82784163ba30450d2379e3c740a21d11202
spoodr <70950932+IBgreat1@users.noreply.github.com>
2022-05-18 15:41:35 +0530

    Update main.go

--- main.go
+++ main.go
@@ -70,10 +70,8 @@ func makeRequest(ctx *fasthttp.RequestCtx, attempt int) *fasthttp.Response {
 	req.Header.SetMethod(string(ctx.Method()))
 	url := strings.SplitN(string(ctx.Request.Header.RequestURI())[1:], "/", 2)
 	if url[0] == "catalog" {
-		fmt.Print("https://roblox.com/"+ url[0] + "/" + url[1])
 		req.SetRequestURI("https://roblox.com/"+ url[0] + "/" + url[1])
 	} else { 
-		fmt.Print("https://" + url[0] + ".roblox.com/" + url[1])
 		req.SetRequestURI("https://" + url[0] + ".roblox.com/" + url[1])
 	}
 	req.SetBody(ctx.Request.Body())
